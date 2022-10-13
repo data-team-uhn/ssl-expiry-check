87df0ea7dc3fc45ccf00f28cef28c7931fab3c76
Michael Lescisin <michael.lescisin@uhn.ca>
2022-10-13 12:35:23 -0400

    Add support for the --warning_threshold argument

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import datetime
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--host', help='The host to check the SSL certificate of (eg. www.google.ca)', required=True)
+argparser.add_argument('--warning_threshold', help="""Only display a certificate expiry warning if the certificate is
+	due to expire in less than or equal to WARNING_THRESHOLD days.""", type=int, default=-1)
 args = argparser.parse_args()
 
 def getSSLCert(hostname, port=443):
@@ -27,4 +29,5 @@ expiry_date = getCertificateDate(ssl_cert, 'notAfter')
 timedelta_until_expiry = expiry_date - datetime.datetime.now()
 days_until_expiry = timedelta_until_expiry.days
 
-print("The certificate will expire in {} days".format(days_until_expiry))
+if (args.warning_threshold < 0) or (days_until_expiry <= args.warning_threshold):
+	print("The certificate will expire in {} days".format(days_until_expiry))
